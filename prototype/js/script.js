cb64d6ed52a795e796a9c0f0176996dac6548bff
Michael Alicea <mfalicea58@gmail.com>
2016-11-07 21:25:25 -0500

    Can add to queue

--- prototype/js/script.js
+++ prototype/js/script.js
@@ -1,10 +1,22 @@
 var widgets = [];
 var queue = [];
+var index = 0;
 
 addWidget(287528029);
 addWidget(288700921);
 
 function addWidget(trackId) {
+  var div = document.createElement('div');
+  div.id = trackId;
+  div.className = 'widget';
+
+  var addQueueBtn = document.createElement('button');
+  addQueueBtn.type = 'button';
+  addQueueBtn.addEventListener('click',function() { addTrackToQueue(widget) });
+  addQueueBtn.innerHTML = 'Add To Queue';
+
+  var widgetsDiv = document.getElementById('widgets');
+
   var widget = { };
   widget.id = trackId;
   widget.iframe = document.createElement('iframe');
@@ -12,19 +24,47 @@ function addWidget(trackId) {
     'http://api.soundcloud.com/tracks/' 
     + trackId + 
     '&auto_play=false&single_active=false';
-
   widget.scWidget = SC.Widget(widget.iframe);
 
+  addListeners(widget);
 
-  widget.scWidget.bind(SC.Widget.Events.PLAY, function(eventData) {
-    console.log('Playing Widget2');
-  });
+  div.appendChild(widget.iframe);
+  div.appendChild(addQueueBtn);
 
-  var widgetDiv = document.getElementById('widgets');
-  widgetDiv.appendChild(widget.iframe);
+  widgetsDiv.appendChild(div);
 
   widgets.push(widget);
   return widget;
+}
+
+function addListeners(widget) {
+  widget.scWidget.bind(SC.Widget.Events.PLAY, function(eventData) {
+    console.log('Playing Widget');
+    widget.eventData = eventData;
+  });
+
+  widget.scWidget.bind(SC.Widget.Events.PLAY_PROGRESS, function(eventData) {
+    widget.eventData = eventData;
+  });
+
+  widget.scWidget.bind(SC.Widget.Events.FINISH, function(eventData) {
+    widget.eventData = eventData;
+    playNextInQueue();
+  });
+}
+
+function addTrackToQueue(widget) {
+  console.log(widget);
+  queue.push(widget);
+}
+
+function playNextInQueue() {
+  index ++;
+  playById(queue[index].id);
+}
+
+function playQueue() {
+  playById(queue[index].id);
 }
 
 function playAll() {
@@ -36,5 +76,39 @@ function playAll() {
 function pauseAll() {
   widgets.forEach(widget => {
     widget.scWidget.pause();
+  });
+}
+
+function playById(trackId) {
+  var widget = getWidget(trackId);
+
+  if (widget) {
+    widget.scWidget.play();
+    return true;
+  } else 
+    return false;
+}
+
+function getWidget(trackId) {
+  return widgets.find(widget => {
+    return widget.id === trackId;
+  });
+}
+
+function getPosition(trackId) {
+  var widget = getWidget(trackId);
+  return widget.scWidget.getPosition(item => {
+    console.log(item);
+    return item;
+  });
+}
+
+function getDuration(trackId) {
+  var widget = getWidget(trackId);
+  let dur = 0;
+  widget.scWidget.getDuration(item => {
+    console.log(item);
+    dur = item;
+    return item;
   });
 }
